3370d6aafca2b73004542dc41c362f4784940bd3
ManssonMarcus <marma363@student.liu.se>
2016-05-14 18:35:19 +0200

    css fixes

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
-//First
+$(document).ready(function(){
+	$('.startTestButton').click(function() {
+		console.log("start button clicked");
+    	$('.questionBox').animate({opacity: '0.1'}, "slow");
+	});
+});
